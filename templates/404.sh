4fd237756491e3628002856de7686ca612140c95
Vinicius Gomes <vinigomescunha@gmail.com>
2020-11-07 06:54:03 -0300

    feat: add default 404

---
+++ templates/404.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "<h1>404 Not Found</h1><hr>"
+echo -e "<div>Oops! Not Found<br><br>The requested resource was not found</div><br><i>${query[file]}</i><hr><i>`Servidorzinho.get_server_info`</i><hr>"
